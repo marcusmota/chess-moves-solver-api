871348989e05f65ae31f261ca8037c84b2dc81b1
Marcus <marcushcsm@gmail.com>
2018-12-24 11:18:47 -0300

    added full knight moves tests

--- test/integration/piece.knight.js
+++ test/integration/piece.knight.js
@@ -13,7 +13,7 @@ describe('Knight Available Moves Unit Test', () => {
         rows : 8
     };
 
-    describe('test moves methods', () => {
+    describe('test KNIGHT moves methods', () => {
 
         it('should moves from A1 to B3 (UP -> UP -> RIGHT)', done => {
 
@@ -162,6 +162,154 @@ describe('Knight Available Moves Unit Test', () => {
 
     });
 
+    describe('test FULL KNIGHT moves methods', () => {
+
+        it('should moves from A1 to B3 (UP -> UP -> RIGHT)', done => {
+
+            const result = knight.UURFull('A1', chessHash);
+
+            expect(result).to.deep.equal(['A2','A3','B3']);
+            done();
+
+        });
+
+        it('should moves from B1 to C3 (UP -> UP -> RIGHT)', done => {
+
+            const result = knight.UURFull('B1', chessHash);
+
+            expect(result).to.deep.equal(['B2','B3','C3']);
+            done();
+
+        });
+
+        it('should moves from B1 to A3 (UP -> UP -> LEFT)', done => {
+
+            const result = knight.UULFull('B1', chessHash);
+
+            expect(result).to.deep.equal(['B2','B3','A3']);
+            done();
+
+        });
+
+        it('should moves from G1 to F3 (UP -> UP -> LEFT)', done => {
+
+            const result = knight.UULFull('G1', chessHash);
+
+            expect(result).to.deep.equal(['G2','G3','F3']);
+            done();
+
+        });
+
+        it('should moves from B3 to A1 (DOWN -> DOWN -> LEFT)', done => {
+
+            const result = knight.DDLFull('B3', chessHash);
+
+            expect(result).to.deep.equal(['B2','B1','A1']);
+            done();
+
+        });
+
+        it('should moves from G3 to F1 (DOWN -> DOWN -> LEFT)', done => {
+
+            const result = knight.DDLFull('G3', chessHash);
+
+            expect(result).to.deep.equal(['G2','G1','F1']);
+            done();
+
+        });
+
+        it('should moves from B3 to C1 (DOWN -> DOWN -> RIGHT)', done => {
+
+            const result = knight.DDRFull('B3', chessHash);
+
+            expect(result).to.deep.equal(['B2','B1','C1']);
+            done();
+
+        });
+
+        it('should moves from G3 to H1 (DOWN -> DOWN -> RIGHT)', done => {
+
+            const result = knight.DDRFull('G3', chessHash);
+
+            expect(result).to.deep.equal(['G2','G1','H1']);
+            done();
+
+        });
+
+        it('should moves from B3 to D4 (RIGHT -> RIGHT -> UP)', done => {
+
+            const result = knight.RRUFull('B3', chessHash);
+
+            expect(result).to.deep.equal(['C3','D3','D4']);
+            done();
+
+        });
+
+        it('should moves from F3 to H4 (RIGHT -> RIGHT -> UP)', done => {
+
+            const result = knight.RRUFull('F3', chessHash);
+
+            expect(result).to.deep.equal(['G3','H3','H4']);
+            done();
+
+        });
+
+        it('should moves from B3 to D2 (RIGHT -> RIGHT -> DOWN)', done => {
+
+            const result = knight.RRDFull('B3', chessHash);
+
+            expect(result).to.deep.equal(['C3','D3','D2']);
+            done();
+
+        });
+
+        it('should moves from F3 to H2 (RIGHT -> RIGHT -> DOWN)', done => {
+
+            const result = knight.RRDFull('F3', chessHash);
+
+            expect(result).to.deep.equal(['G3','H3','H2']);
+            done();
+
+        });
+
+        it('should moves from C3 to A2 (LEFT -> LEFT -> DOWN)', done => {
+
+            const result = knight.LLDFull('C3', chessHash);
+
+            expect(result).to.deep.equal(['B3','A3','A2']);
+            done();
+
+        });
+
+        it('should moves from F3 to D2 (LEFT -> LEFT -> DOWN)', done => {
+
+            const result = knight.LLDFull('F3', chessHash);
+
+            expect(result).to.deep.equal(['E3','D3','D2']);
+            done();
+
+        });
+
+        it('should moves from C3 to A4 (LEFT -> LEFT -> UP)', done => {
+
+            const result = knight.LLUFull('C3', chessHash);
+
+            expect(result).to.deep.equal(['B3','A3','A4']);
+            done();
+
+        });
+
+        it('shouUmoves from F3 to D4 (LEFT -> LEFT -> UP)', done => {
+
+            const result = knight.LLUFull('F3', chessHash);
+
+            expect(result).to.deep.equal(['E3','D3','D4']);
+            done();
+
+        });
+
+
+    });
 
 
 });
