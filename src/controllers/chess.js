b48718bc725d69dfe04796dc684c124ba2da550a
Marcus <marcushcsm@gmail.com>
2018-12-29 11:13:09 -0300

    created the getCouldMove method and few updates on getAvailableMovesByPosition

--- src/controllers/chess.js
+++ src/controllers/chess.js
@@ -77,6 +77,92 @@ const calculateMoves = (piece, position) => {
     return arrayOfMoves;
 };
 
+const getCouldMove = (req, res) => {
+
+    let start = req.query.start ? req.query.start.toUpperCase() : req.query.start;
+    let end = req.query.end ? req.query.end.toUpperCase() : req.query.end;
+    let piece = req.params.piece ? req.params.piece.toUpperCase() : req.params.piece;
+    
+    if(!(/^[a-h|A-H]([0-8]{1})$/).test(start)){
+        return res.status(422).send({msg : "Please, given a valid chess position. Ex.: A1, C8, H3..."})
+    }
+
+    if(!(/^[a-h|A-H]([0-8]{1})$/).test(end)){
+        return res.status(422).send({msg : "Please, given a valid chess position. Ex.: A1, C8, H3..."})
+    }
+
+    return res.send(couldMove(piece, start, end));
+
+};
+
+const couldMove = (piece, start, end) => {
+
+    if(piece === 'KNIGHT'){
+
+        let moves = {
+            uur : true,
+            uul : true,
+            ddr : true,
+            ddl : true,
+            rru : true,
+            rrd : true,
+            llu : true,
+            lld : true
+        };
+        
+        let letter = helpers.getFirstPositionCharacter(start);
+        let num = helpers.getSecondPositionCharacter(start);
+
+        if(letter === 'A' ){
+            delete moves.uul;
+            delete moves.ddl;
+            delete moves.llu;
+            delete moves.lld;
+        }else if(letter === 'B'){
+            delete moves.lld;
+            delete moves.llu;
+        }else if(letter === 'G'){
+            delete moves.rrd;
+            delete moves.rru;
+        }else if(letter === 'H'){
+            delete moves.ddr;
+            delete moves.uur;
+            delete moves.rru;
+            delete moves.rrd;
+        }
+
+        if(num === 1){
+            delete moves.rrd;
+            delete moves.lld;
+            delete moves.ddr;
+            delete moves.ddl;
+        }else if(num === 2){
+            delete moves.ddr;
+            delete moves.ddl;
+        }else if(num === 7){
+            delete moves.uur;
+            delete moves.uul;
+        }else if(num === 8){
+            delete moves.rru;
+            delete moves.llu;
+            delete moves.uur;
+            delete moves.uul;
+        }
+        
+        availableMoves = knightPiece.getKnightMovesLastCell(moves, start, chessHash);
+    
+    }else{
+        return res.status(422).send({msg : "Please, given a valid chess piece. Ex.: Knight"})
+    }
+
+    let arrayOfMoves = [];
+    
+    Object.keys(availableMoves).map(f => {
+        arrayOfMoves.push(availableMoves[f])
+    });
+
+    return arrayOfMoves.indexOf(end) !== -1 ? true : false;
+};
 
 const getAvailableMovesByPosition = (req,res) => {
 
@@ -84,7 +170,7 @@ const getAvailableMovesByPosition = (req,res) => {
 
     let piece = req.params.piece ? req.params.piece.toUpperCase() : req.params.piece;
 
-    let times = req.query.times ? req.query.times : 1;
+    let turns = req.query.turns ? req.query.turns : 1;
     
     if(!(/^[a-h|A-H]([0-8]{1})$/).test(position)){
         return res.status(422).send({msg : "Please, given a valid chess position. Ex.: A1, C8, H3..."})
@@ -98,9 +184,11 @@ const getAvailableMovesByPosition = (req,res) => {
 
     resultBkp = result;
 
-    for(let m=1;m<times;m++){
+    let tmpArr = result;
 
-        let tmpArr = [];
+    for(let m=1;m<turns;m++){
+
+        tmpArr = [];
 
         result.forEach(v => {
 
@@ -114,7 +202,7 @@ const getAvailableMovesByPosition = (req,res) => {
         result = tmpArr;
     }
 
-    result = resultBkp;
+    result = tmpArr;
 
     res.send(result);
 
@@ -125,5 +213,6 @@ const getLastPosition = (arr) => {
 };
 
 module.exports = {
-    getAvailableMovesByPosition
+    getAvailableMovesByPosition,
+    getCouldMove
 }
