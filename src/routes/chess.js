8c8064896e01307e9a47ffa2af8be5eaa70ce7cb
Marcus <marcushcsm@gmail.com>
2018-12-29 11:14:16 -0300

    created the getKnightMovesLastCell method

--- src/routes/chess.js
+++ src/routes/chess.js
@@ -3,6 +3,7 @@ const chessController = require("./../controllers/chess");
 module.exports = function(app){
 
     app.get('/v1/get-moves-by-position/:piece', chessController.getAvailableMovesByPosition);
+    app.get('/v1/could-move/:piece', chessController.getCouldMove);
   
 };
 
